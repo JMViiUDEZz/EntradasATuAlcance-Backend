43fd92bc80d663f438081dcb4835b7fd49e0ae57
José María Viúdez <jmviudezp2003@gmail.com>
2023-03-13 10:43:52 +0100

    another commit

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /app
-cp /tmp/environments/.env ./.env
-npx prisma generate
-npm run start
--- docker-nginx.sh
+++
@@ -1,2 +0,0 @@
-docker build -t my-nginx .
-docker run -d -p 81:80 --link my-nest-app:nest-app my-nginx
